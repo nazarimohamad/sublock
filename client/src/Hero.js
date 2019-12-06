6f898e3ad95d41fbd3e623236f48ddd6fcc105f2
Ryan Nazari <ryannazari21@gmail.com>
2019-12-06 11:37:38 +0200

    fixing the carousel blur image and height of wipes components

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -5,12 +5,6 @@ import './style.scss';
 import fashion01 from './images/fashion07.jpeg';
 import fashion02 from './images/fashion02.jpg';
 import fashion05 from './images/fashion05.jpeg';
-// import fashion4 from './images/fashion4.jpeg';
-// import fashion5 from './images/fashion5.jpg';
-// import fashion6 from './images/fashion6.jpeg';
-// import fashion7 from './images/fashion 7.jpeg';
-// import sus3 from './images/sus3.jpg';
-// import sus6 from './images/sus6.jpg';
 
 
 class Hero extends Component {
@@ -60,15 +54,15 @@ class Hero extends Component {
             interval={4000}
             transitionTime={400}>
               <div>
-                  <img src={fashion05} className='image'/>
+                  <img src={fashion05} alt={fashion05} className='image'/>
                   <p className="legend" style={{color:'rgb(4, 43, 46)'}}>Legend one says that i=this is icons for the best icon 1</p>
               </div>
               <div>
-                  <img src={fashion02} className='image'/>
+                  <img src={fashion02} alt={fashion02} className='image'/>
                   <p className="legend" style={{color:'white !important'}}>Legend is about sex shop in the resturant and fuck the beautiful girl 2</p>
               </div>
               <div>
-                  <img src={fashion01} className='image'/>
+                  <img src={fashion01} alt={fashion01} className='image'/>
                   <p className="legend" style={{color:'white'}}>Legend is nothing just another sex with estonian girls in the street3</p>
               </div>
           </Carousel>
