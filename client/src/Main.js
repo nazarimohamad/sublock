d94c79ba1c93f847e2dc0b79f3220662d49313b1
Ryan Nazari <ryannazari21@gmail.com>
2019-12-11 14:20:53 +0200

    add terms & condition and privacy policy

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Home';
-import { ParallaxProvider } from 'react-scroll-parallax';
+// import { ParallaxProvider } from 'react-scroll-parallax';
+import Terms from './Terms';
+import PrivacyPolicy from './PrivacyPolicy'
+
+
 
 const Main = () => {
   return(
@@ -9,6 +13,8 @@ const Main = () => {
       <Router>
         <Switch>
           <Route exact path='/' render={(props) => <Home {...props}/>} />
+          <Route path='/terms-and-conditions' render={props => <Terms {...props} />} />
+          <Route path='/privacy-policy' render={props => <PrivacyPolicy {...props} />}/>
         </Switch>
       </Router>
     // </ParallaxProvider>
